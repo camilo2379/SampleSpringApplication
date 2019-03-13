0617c25033dc315ad9cc4cb3c3f53f4a55c40170
Camilo Rojas <kmilo-rojas@hotmail.com>
2019-03-13 00:05:54 -0500

    configuracion

--- src/java/co/edu/uniminuto/pa/bds/Test_MySqlDataSource.java
+++ src/java/co/edu/uniminuto/pa/bds/Test_MySqlDataSource.java
@@ -16,7 +16,7 @@ public class Test_MySqlDataSource {
 
 // Setting up the DataSource object
       PGPoolingDataSource ds = new PGPoolingDataSource();
-      ds.setServerName("vehialpes");
+      ds.setURL("postgres://sbmbfcandawcln:cc9a9e7077f4fba8ca8a2a057b6d33c7b310182b987e0d63f827bc1c275039b2@ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/db2uo3hp5f3du1");
       ds.setPortNumber(5432);
       ds.setDatabaseName("db2uo3hp5f3du1");
       ds.setUser("sbmbfcandawcln");

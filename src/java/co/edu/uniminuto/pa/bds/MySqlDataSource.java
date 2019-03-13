0e2a4e8671a70bfd3fe98ae1ece533b82f4806b2
Camilo Rojas <kmilo-rojas@hotmail.com>
2019-03-12 23:45:22 -0500

    configuracion

--- src/java/co/edu/uniminuto/pa/bds/MySqlDataSource.java
+++ src/java/co/edu/uniminuto/pa/bds/MySqlDataSource.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package co.edu.uniminuto.pa.bds;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -16,13 +15,13 @@ import org.postgresql.ds.PGPoolingDataSource;
  * @author nixoduaa
  */
 public class MySqlDataSource {
-    private static PGPoolingDataSource miMySqlDS = null;
-    private static PGPoolingDataSource ds = null;
+    private static MySqlDataSource miMySqlDS = null;
+    private static  PGPoolingDataSource ds = null;
     
     private MySqlDataSource(){
         ds = new PGPoolingDataSource();
         ds.setServerName("vehialpes");
-        ds.setPortNumber(5492);
+        ds.setPortNumber(5432);
         ds.setDatabaseName("db2uo3hp5f3du1");
         ds.setUser("sbmbfcandawcln");
         ds.setPassword("cc9a9e7077f4fba8ca8a2a057b6d33c7b310182b987e0d63f827bc1c275039b2");        
@@ -33,7 +32,7 @@ public class MySqlDataSource {
         try {
             if (miMySqlDS == null)
             {
-                miMySqlDS = new PGPoolingDataSource();            
+                miMySqlDS = new MySqlDataSource();            
             }
             
             return ds.getConnection();
--- src/java/co/edu/uniminuto/pa/bds/Test_MySqlDataSource.java
+++ src/java/co/edu/uniminuto/pa/bds/Test_MySqlDataSource.java
@@ -11,25 +11,32 @@ import org.postgresql.ds.PGPoolingDataSource;
 public class Test_MySqlDataSource {
     
   public static void main(String [] args) {
-     PGPoolingDataSource source = new PGPoolingDataSource();
-    source.setURL("postgres://sbmbfcandawcln:cc9a9e7077f4fba8ca8a2a057b6d33c7b310182b987e0d63f827bc1c275039b2@ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/db2uo3hp5f3du1");
-    source.setServerName("vehialpes");
-    source.setDatabaseName("db2uo3hp5f3du1");
-    source.setUser("sbmbfcandawcln");
-    source.setPassword("cc9a9e7077f4fba8ca8a2a057b6d33c7b310182b987e0d63f827bc1c275039b2");
-    source.setMaxConnections(10);
     Connection con = null;
     try {
 
 // Setting up the DataSource object
-       con = source.getConnection();
-    // use connection
-} catch (SQLException e) {
-    // log error
-} finally {
-    if (con != null) {
-        try { con.close(); } catch (SQLException e) {}
-    }
+      PGPoolingDataSource ds = new PGPoolingDataSource();
+      ds.setServerName("vehialpes");
+      ds.setPortNumber(5432);
+      ds.setDatabaseName("db2uo3hp5f3du1");
+      ds.setUser("sbmbfcandawcln");
+      ds.setPassword("cc9a9e7077f4fba8ca8a2a057b6d33c7b310182b987e0d63f827bc1c275039b2");
+
+// Getting a connection object
+      con = ds.getConnection();
+      
+// Getting database info
+      DatabaseMetaData meta = con.getMetaData();
+      System.out.println("Server name: " 
+        + meta.getDatabaseProductName());
+      System.out.println("Server version: "
+        + meta.getDatabaseProductVersion());
+
+// Closing the connection
+      con.close();
+    } catch (Exception e) {
+      System.err.println("Exception: "+e.getMessage());
     }
   }
 }
+

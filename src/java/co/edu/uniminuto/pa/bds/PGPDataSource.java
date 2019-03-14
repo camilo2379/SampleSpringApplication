8c8949160082e6c2794123175400f7807a6eac07
Camilo Rojas <kmilo-rojas@hotmail.com>
2019-03-13 23:07:36 -0500

    configuracion

--- src/java/co/edu/uniminuto/pa/bds/PGPDataSource.java
+++ src/java/co/edu/uniminuto/pa/bds/PGPDataSource.java
@@ -4,23 +4,24 @@
  * and open the template in the editor.
  */
 package co.edu.uniminuto.pa.bds;
+import org.postgresql.ds.PGPoolingDataSource;
+//import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.postgresql.ds.PGPoolingDataSource;
 
 /**
  *
  * @author nixoduaa
  */
-public class MySqlDataSource {
-    private static MySqlDataSource miMySqlDS = null;
-    private static  PGPoolingDataSource ds = null;
+public class PGPDataSource {
+    private static PGPDataSource miMySqlDS = null;
+    private static PGPoolingDataSource ds = null;
     
-    private MySqlDataSource(){
+    private PGPDataSource(){
         ds = new PGPoolingDataSource();
-        ds.setURL("postgres://sbmbfcandawcln:cc9a9e7077f4fba8ca8a2a057b6d33c7b310182b987e0d63f827bc1c275039b2@ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/db2uo3hp5f3du1");
+        ds.setServerName("ec2-54-247-70-127.eu-west-1.compute.amazonaws.com");
         ds.setPortNumber(5432);
         ds.setDatabaseName("db2uo3hp5f3du1");
         ds.setUser("sbmbfcandawcln");
@@ -32,13 +33,13 @@ public class MySqlDataSource {
         try {
             if (miMySqlDS == null)
             {
-                miMySqlDS = new MySqlDataSource();            
+                miMySqlDS = new PGPDataSource();            
             }
             
             return ds.getConnection();
             
         } catch (SQLException ex) {
-            Logger.getLogger(MySqlDataSource.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(PGPDataSource.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         return null;
